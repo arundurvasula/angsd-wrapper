96af67d1281781712ea9433182b1fcc7e4899a72
tvkent <tvkent@ucdavis.edu>
2015-06-25 22:53:13 -0700

    fixed reactive chr

--- scripts/shiny/ui.R
+++ scripts/shiny/ui.R
@@ -8,44 +8,45 @@ shinyUI(fluidPage(
       "Thetas",
       sidebarLayout(
         sidebarPanel(
-            fileInput('userThetas',
-                      label= 'Choose Thetas File'
-            ),
-            
-            selectInput("thetaChoice",
-                        label = "Choose estimator of theta to graph", 
-                        choices = c("Watterson's Theta", "Pairwise Theta", "Fu and Li's Theta", "Fay's Theta", "Maximum likelihood (L) Theta"),
-                        selected = "Watterson's Theta"
-            ),
-            selectInput("selectionChoice",
-                        label = "Choose a neutrality test statistic to graph", 
-                        choices = c("Tajima's D", "Fi and Li's D", "Fu and Li's F", "Fay and Wu's H", "Zeng's E"),
-                        selected = "Tajima's D"
-            ),
-            hr(),
-            checkboxInput("thetaLowess","Theta Lowess", value=FALSE),
-            checkboxInput("selectionLowess","Neutrality Test Statistic Lowess", value=FALSE),
-            hr(),
-            numericInput("WinCenterLow", "Base Start Position", value=0),
-            numericInput("WinCenterHigh", "Base End Position", value=10000),
-            checkboxInput("subset","Toggle subset data", value=FALSE),
-            hr(),
-            checkboxInput("rm.nsites", "Remove data where nSites < x", value=FALSE),
-            numericInput("nsites", "x:",value=0),
-            
-            hr(),
-            fileInput('userAnnotations',
-                      label= 'Choose GFF File'
-            ),
-            checkboxInput("annotations","Toggle GFF annotations", value=FALSE)
+          fileInput('userThetas',
+                    label= 'Choose Thetas File'
           ),
+          
+          selectInput("thetaChoice",
+                      label = "Choose estimator of theta to graph", 
+                      choices = c("Watterson's Theta", "Pairwise Theta", "Fu and Li's Theta", "Fay's Theta", "Maximum likelihood (L) Theta"),
+                      selected = "Watterson's Theta"
+          ),
+          selectInput("selectionChoice",
+                      label = "Choose a neutrality test statistic to graph", 
+                      choices = c("Tajima's D", "Fi and Li's D", "Fu and Li's F", "Fay and Wu's H", "Zeng's E"),
+                      selected = "Tajima's D"
+          ),
+          uiOutput('thetaChroms'),
+          hr(),
+          checkboxInput("thetaLowess","Theta Lowess", value=FALSE),
+          checkboxInput("selectionLowess","Neutrality Test Statistic Lowess", value=FALSE),
+          hr(),
+          numericInput("WinCenterLow", "Base Start Position", value=0),
+          numericInput("WinCenterHigh", "Base End Position", value=10000),
+          checkboxInput("subset","Toggle subset data", value=FALSE),
+          hr(),
+          checkboxInput("rm.nsites", "Remove data where nSites < x", value=FALSE),
+          numericInput("nsites", "x:",value=0),
+          
+          hr(),
+          fileInput('userAnnotations',
+                    label= 'Choose GFF File'
+          ),
+          checkboxInput("annotations","Toggle GFF annotations", value=FALSE)
+        ),
         # Show a plot of the thetas
-          mainPanel(
-              plotOutput("thetaPlot"), 
-              plotOutput("selectionPlot")
-          )
+        mainPanel(
+          plotOutput("thetaPlot"), 
+          plotOutput("selectionPlot")
         )
-      ),
+      )
+    ),
     tabPanel(
       "SFS",
       sidebarLayout(
@@ -88,7 +89,7 @@ shinyUI(fluidPage(
           fileInput('userIntersect',
                     label= 'Choose Intersect File'
           ),
-       
+          
           uiOutput('fstChroms'),
           
           hr(),
@@ -159,6 +160,6 @@ shinyUI(fluidPage(
       )
     )
     #end of tabsetPanel
-    )
   )
+)
 )
